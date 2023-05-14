7a13488b481117238130a8d0c753d049d25bfdeb
Gustavo <gustavo.cilleruelo@gmail.com>
2023-05-14 21:52:18 +0100

    Figures a pygame!

--- reconocimiento2D.py
+++ reconocimiento2D.py
@@ -5,8 +5,10 @@ import numpy as np
 cap = cv2.VideoCapture(0)  # Use the first available camera
 
 # Define the lower and upper bounds for the color of the shapes you want to detect
-lower_color = np.array([0, 0, 0])  # Replace with the lower bounds for the color
-upper_color = np.array([255, 255, 255])  # Replace with the upper bounds for the color
+# Replace with the lower bounds for the color
+lower_color = np.array([0, 0, 0])
+# Replace with the upper bounds for the color
+upper_color = np.array([255, 255, 255])
 
 # Create an empty dictionary to store the detected shapes
 shapes = {}
@@ -23,20 +25,24 @@ while True:
     edges = cv2.Canny(blurred, 50, 150)
 
     # Find the contours of the shapes
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(
+        edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Iterate through each contour and store the data
     for i, contour in enumerate(contours):
         area = cv2.contourArea(contour)  # Get the area of the contour
-        perimeter = cv2.arcLength(contour, True)  # Get the perimeter of the contour
-        x, y, w, h = cv2.boundingRect(contour)  # Get the bounding box of the contour
+        # Get the perimeter of the contour
+        perimeter = cv2.arcLength(contour, True)
+        # Get the bounding box of the contour
+        x, y, w, h = cv2.boundingRect(contour)
 
         # Draw the bounding box and contour on the original frame
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.drawContours(frame, [contour], 0, (0, 0, 255), 2)
 
         # Store the data in the dictionary
-        shape_data = {"area": area, "perimeter": perimeter, "x": x, "y": y, "width": w, "height": h}
+        shape_data = {"area": area, "perimeter": perimeter,
+                      "x": x, "y": y, "width": w, "height": h}
         shapes[f"shape{i+1}"] = shape_data
 
     # Display the original frame with the contours and bounding boxes
